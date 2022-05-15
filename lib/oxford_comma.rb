8437788a366965e15fd90e633d83f4c8becd9bd1
Bogdan Cuza <bogdancuza@protonmail.com>
2022-05-16 00:09:41 +0300

    commit tree 15f1c07fbe5f3b2f6422d4929e7393104ddc05e6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array[0]
+    elsif array.size == 2
+        "#{array.first} and #{array.last}"
+    else
+        array.first(array.size-1).join(", ") << ", and #{array.last}"
+    end
 end
